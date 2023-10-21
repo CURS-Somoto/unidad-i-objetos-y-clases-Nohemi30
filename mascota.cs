807b14ea563fa6620f91a2608824729a01fc684b
Nohemi30 <147261816+Nohemi30@users.noreply.github.com>
2023-10-20 21:31:16 -0600

    mascota.cs
    trabajo entregado

--- mascota.cs
+++ mascota.cs
@@ -1,14 +1,54 @@
 using System;
 
-public class Mascota
+class Mascota
 {
-     
+    public string Nombre { get; set; }
+    public string Tipo { get; set; }
+    public int Edad { get; set; }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Tipo: {Tipo}");
+        Console.WriteLine($"Edad: {Edad} años");
+        Console.WriteLine();
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Mascota mascota1 = new Mascota()
+        {
+            Nombre = "Max",
+            Tipo = "Perro",
+            Edad =5
+        };
+
+        Mascota mascota2 = new Mascota()
+        {
+            Nombre = "Mariposa",
+            Tipo = "Gato",
+            Edad = 1
+        };
+
+        Mascota mascota3 = new Mascota()
+        {
+            Nombre = "Tepol",
+            Tipo = "Pez",
+            Edad = 2
+        };
+
+        mascota1.MostrarInformacion();
+        mascota2.MostrarInformacion();
+        mascota3.MostrarInformacion();
+
+        Console.ReadLine();
     }
 }
+
+
+
+
+
